152c3920e4ef2c1bf3d89481b520d626bffaa931
Moroz07 <morozov-f.morozov@yandex.ru>
2026-04-20 09:27:58 +0300

    добавлены тесты. реализую

---
+++ Theater.Test/Model/TicketTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theater.Test.Model
+{
+    internal class TicketTest
+    {
+        [Fact]
+        public void Book_WithValidData_ShouldBeValid()
+        {
+            // Создаем объект книги с валидными значениями.
+            var book = new Ticket
+            {
+                Name = "C# in Depth",     // Обязательное поле, строка < 100 символов
+                Author = new Author { Name = "Пушкин" },      // Обязательное поле, строка < 100 символов
+                Year = 2020,               // В пределах допустимого диапазона 1000–2100
+            };
+
+            // Создаем контекст валидации на основе объекта
+            var context = new ValidationContext(book);
+
+            // Сюда будут записаны ошибки валидации, если они есть
+            var result = new List<ValidationResult>();
+
+            // Проводим валидацию объекта с учетом всех атрибутов [Required], [Range] и т.п.
+            var isValid = Validator.TryValidateObject(book, context, result, true);
+
+            // Ожидаем, что валидация прошла успешно (все поля корректны)
+            Assert.True(isValid);
+
+            // Также убеждаемся, что список ошибок пуст
+            Assert.Empty(result);
+        }
+    }
+}
---
+++ Theater.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Theater.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
