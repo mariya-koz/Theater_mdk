51e5669d7caf6dc11910c51011ad0808b3346c46
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-04-25 13:49:00 +0300

    сделан тест страницы

---
+++ Theater_mdk.Test/Pages/Ticket/CreateModelTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theater_mdk.Data;
+using Theater_mdk.Models;
+
+namespace Theater_mdk.Test.Pages.Ticket
+{
+    public class CreateModelTest
+    {
+        private ApplicationDBContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDBContext(options);
+        }
+
+        [Fact]
+        public void OnPost_ShouldReturnPage_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var context = GetDbContext();
+            var pageModel = new Theater_mdk.Pages.Tickets.CreateModel(context);
+
+            pageModel.ModelState.AddModelError("Title", "Required");
+
+            // Act
+            var result = pageModel.OnPost();
+
+            // Assert
+            object value = result.Should().BeOfType<PageResult>();
+            context.Ticket.Count().Should().Be(0);
+        }
+    }
+}
