1907441c4a7f8a100e9f45ece256c784423e3254
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-04-25 14:01:23 +0300

    тест hub

---
+++ Theater_mdk.Test/Hubs/TicketHubTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theater_mdk.Hubs;
+using Theater_mdk.Models;
+
+namespace Theater_mdk.Test.Hubs
+{
+    public class TicketHubTests
+    {
+        [Fact]
+        public async Task SendTicketUpdate_ShouldSendMessageToAllClients()
+        {
+            // Arrange
+            var hub = new TicketHub();
+
+            var clientsMock = new Mock<IHubCallerClients>();
+            var clientProxyMock = new Mock<IClientProxy>();
+
+            clientsMock.Setup(c => c.All).Returns(clientProxyMock.Object);
+
+            hub.Clients = clientsMock.Object;
+
+            var ticket = new Ticket
+            {
+                Title = "Test Ticket",
+                DataShow = new DateTime(2001, 02, 20),
+                Price = 1500
+            };
+
+            // Act
+            await hub.SendTicketUpdate(ticket);
+
+            // Assert
+            clientProxyMock.Verify(
+                c => c.SendCoreAsync(
+                    "TicketUpdated",
+                    It.Is<object[]>(o => o.Length == 1 && o[0] == ticket),
+                    default
+                ),
+                Times.Once
+            );
+        }
+
+    }
+}
---
+++ Theater_mdk/Hubs/TicketHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Theater_mdk.Models;
+
+namespace Theater_mdk.Hubs
+{
+    public class TicketHub : Hub
+    {
+        public async Task SendTicketUpdate(Ticket ticket)
+        {
+            await Clients.All.SendAsync("TicketUpdated", ticket);
+        }
+
+    }
+}
