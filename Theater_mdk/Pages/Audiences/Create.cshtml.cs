71e0535d87f94ff434b63227d3759a31cb4cf7a9
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-04-30 08:33:33 +0300

    авторизация доделана

--- Theater_mdk/Pages/Audiences/Create.cshtml.cs
+++ Theater_mdk/Pages/Audiences/Create.cshtml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Theater_mdk.Data;
--- Theater_mdk/Pages/Index.cshtml.cs
+++ Theater_mdk/Pages/Index.cshtml.cs
@@ -1,15 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Theater_mdk.Data;
+using Theater_mdk.Models;
 
 namespace Theater_mdk.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDBContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
